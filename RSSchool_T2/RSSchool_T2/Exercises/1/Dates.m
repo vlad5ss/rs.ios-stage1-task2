d23c092eb96981ebe448964fe38aa9b289b08f0e
vlad5ss <vlad5grup@gmail.com>
2020-03-22 00:49:03 +0400

    Add:exercise 1 dates
    Second commit

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -11,8 +11,26 @@
     [dateFormat setDateFormat:@"ddMMyyyy"];
     NSDate *date = [dateFormat dateFromString:dateStr];
     
+//        [dateFormat setDateFormat:@"EEEE MMMM d, YYYY"];
+//        dateStr = [dateFormat stringFromDate:date];
+    
+    
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setDay: [day intValue]];
+    [comps setMonth:[month intValue]];
+    [comps setYear:[year intValue]];
+    Boolean Validate= comps.isValidDate;
+    if ( (Validate=true)) {
         [dateFormat setDateFormat:@"EEEE MMMM d, YYYY"];
         dateStr = [dateFormat stringFromDate:date];
+    } else {
+        
+        dateStr =@"Date don't exist";
+    }
+    
+    
+    
+    
     return dateStr;
 }
 
