5ac96512f8b90138952624ab78e8e4bf4634a6b1
vlad5ss <vlad5grup@gmail.com>
2020-03-23 00:31:03 +0400

    Add: abreviation exercise 3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,35 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    a = [a lowercaseString];
+    b = [b lowercaseString];
+    
+    NSMutableSet *setA = [NSMutableSet new];
+    NSMutableSet *setB = [NSMutableSet new];
+    NSMutableSet *setDiff = [NSMutableSet new];
+    NSMutableArray *arrayA = [NSMutableArray new];
+    
+    
+    for(int i = 0; i < [a length]; i++){
+        [setA addObject:[a substringWithRange:NSMakeRange(i, 1)]];
+        [arrayA addObject:[a substringWithRange:NSMakeRange(i, 1)]];
+    }
+    
+    for(int i = 0; i < [a length]; i++){
+        [setB addObject:[b substringWithRange:NSMakeRange(i, 1)]];
+    }
+    
+    setDiff = [setA mutableCopy];
+    [setDiff minusSet:setB];
+    [setA minusSet:setDiff];
+    
+    NSEnumerator *enumerator = [setDiff objectEnumerator];
+    NSString *value;
+    while ((value = [enumerator nextObject])) {
+        [arrayA removeObject:value];
+    }
+    
+    return ([[arrayA componentsJoinedByString:@""] isEqualToString:b]) ? @"YES" : @"NO";
 }
 @end
+
