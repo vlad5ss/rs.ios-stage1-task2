22aac9a0bc77b234ca92b473d09023b82fd21445
vlad5ss <vlad5grup@gmail.com>
2020-03-23 22:34:07 +0400

    Add:block task2 exercise 5

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,7 +1,49 @@
 #import "Blocks.h"
 
-@implementation Blocks
+@interface Blocks ()
 
+@property (nonatomic, copy) NSArray *myarray;
+@property (nonatomic, copy) Class myclass;
 
 @end
+
+@implementation Blocks
+
+- (BlockA)blockA {
+    return ^void(NSArray *array) {
+        self.myarray = array;
+    };
+}
+- (BlockB)blockB {
+    return ^void(Class class) {
+        self.myclass = class;
+        NSMutableArray *arraymutable = [self.myarray mutableCopy];
+        [arraymutable removeObjectIdenticalTo:[NSNull null]];
+        [arraymutable filterUsingPredicate:[NSPredicate predicateWithFormat: @"isKindOfClass: %@", class]];
+        
+        if (class == [NSNumber class]) {
+            __block NSInteger numer = 0;
+            [arraymutable enumerateObjectsUsingBlock:^(NSNumber *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                numer += [obj intValue];
+            }];
+            self.blockC(@(numer));
+        } else if (class == [NSString class]) {
+            __block NSString *str = @"";
+            [arraymutable enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                str = [NSString stringWithFormat:@"%@%@", str, obj];
+            }];
+            self.blockC(str);
+        } else if (class == [NSDate class]) {
+            NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+            formatter.dateFormat = @"dd.MM.yyyy";
+            NSDate *resultDate = [arraymutable firstObject];
+            for (NSDate *currentDate in arraymutable) {
+                resultDate = [currentDate laterDate:resultDate];
+            }
+            self.blockC([formatter stringFromDate:resultDate]);
+        }
+    };
+}
+@end
+
 
