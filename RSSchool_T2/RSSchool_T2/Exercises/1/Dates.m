c53a40473519d7e33e4be6ee74fb6265d44d3324
vlad5ss <vlad5grup@gmail.com>
2020-03-23 20:29:08 +0400

    Fix: exercise one task 2 with date

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -4,6 +4,7 @@
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
     //Task1 exxercise
+    //Task1 exxercise
     NSString *concat = [day stringByAppendingString:month];
     NSString *dateStr = [concat stringByAppendingString:year];
     
@@ -11,23 +12,11 @@
     [dateFormat setDateFormat:@"ddMMyyyy"];
     NSDate *date = [dateFormat dateFromString:dateStr];
     
-//        [dateFormat setDateFormat:@"EEEE MMMM d, YYYY"];
-//        dateStr = [dateFormat stringFromDate:date];
-    
-    
-    NSDateComponents *comps = [[NSDateComponents alloc] init];
-    [comps setDay: [day intValue]];
-    [comps setMonth:[month intValue]];
-    [comps setYear:[year intValue]];
-    Boolean Validate= comps.isValidDate;
-    if ( (Validate=true)) {
+    if (!date) { return @"Такого дня не существует"; } else {
         [dateFormat setDateFormat:@"EEEE MMMM d, YYYY"];
         dateStr = [dateFormat stringFromDate:date];
-    } else {
         
-        dateStr =@"Date don't exist";
     }
-    
     
     
     
